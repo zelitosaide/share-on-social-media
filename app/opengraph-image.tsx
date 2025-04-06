a012c92c5adae4f42c3003f5ffa4d52a40a04cdf
Zelito A. Saide <zelitosaide83@gmail.com>
2025-04-06 17:20:34 +0200

    .

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -9,40 +9,7 @@ export const contentType = "image/png"
 
 export default function Image() {
   return new ImageResponse(
-    <div
-      style={{
-        fontSize: 64,
-        background: "linear-gradient(to bottom, #f0f9ff, #e6f7ff)",
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: "40px",
-      }}
-    >
-      <div
-        style={{
-          fontSize: 72,
-          fontWeight: "bold",
-          textAlign: "center",
-          marginBottom: "20px",
-          color: "#0070f3",
-        }}
-      >
-        Dynamic OG Blog by zas
-      </div>
-      <div
-        style={{
-          fontSize: 36,
-          color: "#444",
-          textAlign: "center",
-        }}
-      >
-        Creating dynamic social media images with Next.js
-      </div>
-    </div>,
+    <div>Ola</div>,
     {
       ...size,
     },
